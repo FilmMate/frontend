b1708cc67ef50e387cd6097d37381b1d7431dc03
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2024-04-23 14:32:16 +0530

    refactored

--- lib/core/menu_data.dart
+++ lib/core/menu_data.dart
@@ -5,15 +5,15 @@ import 'package:film_mate/presentation/search/search.dart';
 import 'package:flutter/cupertino.dart';
 
 const List<Widget> screens = [
-      ScreenHome(),
-      ScreenSearch(),
-      ScreenExplore(),
-      ScreenAccounts(),
-    ];
+  ScreenHome(),
+  ScreenSearch(),
+  ScreenExplore(),
+  ScreenAccounts(),
+];
 
 const List<IconData> icons = [
-      CupertinoIcons.home,
-      CupertinoIcons.search,
-      CupertinoIcons.compass,
-      CupertinoIcons.profile_circled,
-    ];
+  CupertinoIcons.home,
+  CupertinoIcons.search,
+  CupertinoIcons.compass,
+  CupertinoIcons.profile_circled,
+];
