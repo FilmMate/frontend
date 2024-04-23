6f82c6e5b55e07ac8c49c22397452efd89e77412
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2024-04-23 14:29:16 +0530

    registered bloc to root

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:film_mate/core/colors.dart';
 import 'package:film_mate/presentation/main_navigator/main_navigator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'application/explore/explore_bloc.dart';
+import 'application/home/home_bloc.dart';
 import 'application/main_navigator/main_navigator_bloc.dart';
 import 'core/di/injectable.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,14 +23,17 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: ((ctx) => getIt<MainNavigatorBloc>())),
+        BlocProvider(create: ((ctx) => getIt<HomeBloc>())),
+        BlocProvider(create: ((ctx) => getIt<ExploreBloc>())),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'FilmMate',
         theme: ThemeData(
-            scaffoldBackgroundColor: kBackgroundColor,
-            useMaterial3: true,
-            ),
+          fontFamily: GoogleFonts.montserrat().fontFamily,
+          scaffoldBackgroundColor: kBackgroundColor,
+          useMaterial3: true,
+        ),
         home: const MainNavigator(),
       ),
     );
