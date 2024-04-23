558cc4b114ca62657759601ced859c3a52738446
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2024-04-23 14:30:57 +0530

    updated constants

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 // Scaffold background color
-const Color kBackgroundColor =  Color.fromARGB(255, 27, 26, 26);
+const Color kBackgroundColor = Color.fromARGB(255, 27, 26, 26);
+
+const Color kBottomNavColor = Color.fromARGB(255, 37, 36, 36);
 
 // Icon background color
 const Color kSelectedBackgroundColor = Color.fromARGB(255, 162, 20, 10);
@@ -10,3 +12,7 @@ const Color kdefaultBackgroundColor = Colors.transparent;
 // Icon color
 final Color kDefaultColor = Colors.grey[800]!;
 const Color kSelectedColor = Colors.white;
+
+// default colors
+const Color kWhite = Colors.white;
+const Color kBlack = Colors.black;
---
+++ lib/core/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kHeightXS = SizedBox(
+  height: 5,
+);
+
+const kHeightS = SizedBox(
+  height: 15,
+);
