a242687bd59bd0ed20b6ec2a0ebb6324a31742fd
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2024-05-01 11:11:02 +0530

    build runner generated files

--- lib/core/di/injectable.config.dart
+++ lib/core/di/injectable.config.dart
@@ -8,15 +8,17 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:film_mate/application/detail/detail_bloc.dart' as _i9;
-import 'package:film_mate/application/explore/explore_bloc.dart' as _i10;
-import 'package:film_mate/application/home/home_bloc.dart' as _i3;
+import 'package:film_mate/application/detail/detail_bloc.dart' as _i10;
+import 'package:film_mate/application/explore/explore_bloc.dart' as _i12;
+import 'package:film_mate/application/home/home_bloc.dart' as _i11;
 import 'package:film_mate/application/main_navigator/main_navigator_bloc.dart'
-    as _i4;
-import 'package:film_mate/domain/services/detail_services.dart' as _i7;
-import 'package:film_mate/domain/services/explore_services.dart' as _i5;
-import 'package:film_mate/infrastructure/detail/impl_detail.dart' as _i8;
-import 'package:film_mate/infrastructure/explore/impl_explore.dart' as _i6;
+    as _i3;
+import 'package:film_mate/domain/services/detail_services.dart' as _i8;
+import 'package:film_mate/domain/services/explore_services.dart' as _i4;
+import 'package:film_mate/domain/services/home_services.dart' as _i6;
+import 'package:film_mate/infrastructure/detail/impl_detail.dart' as _i9;
+import 'package:film_mate/infrastructure/explore/impl_explore.dart' as _i5;
+import 'package:film_mate/infrastructure/home/impl_home.dart' as _i7;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -31,13 +33,15 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i3.HomeBloc>(() => _i3.HomeBloc());
-    gh.factory<_i4.MainNavigatorBloc>(() => _i4.MainNavigatorBloc());
-    gh.lazySingleton<_i5.ExploreServices>(() => _i6.GetLastestImpl());
-    gh.lazySingleton<_i7.DetailServices>(() => _i8.ImplDetail());
-    gh.factory<_i9.DetailBloc>(() => _i9.DetailBloc(gh<_i7.DetailServices>()));
-    gh.factory<_i10.ExploreBloc>(
-        () => _i10.ExploreBloc(gh<_i5.ExploreServices>()));
+    gh.factory<_i3.MainNavigatorBloc>(() => _i3.MainNavigatorBloc());
+    gh.lazySingleton<_i4.ExploreServices>(() => _i5.GetLastestImpl());
+    gh.lazySingleton<_i6.HomeServices>(() => _i7.ImplHome());
+    gh.lazySingleton<_i8.DetailServices>(() => _i9.ImplDetail());
+    gh.factory<_i10.DetailBloc>(
+        () => _i10.DetailBloc(gh<_i8.DetailServices>()));
+    gh.factory<_i11.HomeBloc>(() => _i11.HomeBloc(gh<_i6.HomeServices>()));
+    gh.factory<_i12.ExploreBloc>(
+        () => _i12.ExploreBloc(gh<_i4.ExploreServices>()));
     return this;
   }
 }
