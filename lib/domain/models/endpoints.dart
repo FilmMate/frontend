6f8886b3be4b6511aa4d1d43ba56f7c6d845c1e2
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2024-05-01 11:11:19 +0530

    added new endpoints

--- lib/domain/models/endpoints.dart
+++ lib/domain/models/endpoints.dart
@@ -2,9 +2,13 @@ class EndPoints {
   // flask server base url
   static const serverBaseUrl = "https://filmmate.pythonanywhere.com";
 
-  // get latest endpoint
+  // tmdb base url
+  static const tmdbBaseUrl = "https://api.themoviedb.org/3";
+
+  // get latest movie
   static const latest = "$serverBaseUrl/getlatest";
 
+  // get latest tv
   static const getLatestTV = "$serverBaseUrl/getlatesttv";
 
   // get details
@@ -13,4 +17,22 @@ class EndPoints {
 
   //image endpoints
   static const image = 'https://image.tmdb.org/t/p/original';
+
+  // tmdb now playing movies
+  static const getNowPlaying = "$tmdbBaseUrl/movie/now_playing";
+
+  //tmdb trending movie
+  static const getTrendingMovie = "$tmdbBaseUrl/trending/movie/day";
+
+  //tmdb trending tv
+  static const getTrendingTv = "$tmdbBaseUrl/trending/tv/day";
+
+  //tmdb top rated movies
+  static const topRatedMovies = "$tmdbBaseUrl/movie/top_rated";
+
+  //tmdb top rated tv
+  static const topRatedTV = "$tmdbBaseUrl/tv/top_rated";
+
+  //tmdb search
+  static const search = "$tmdbBaseUrl/search/multi";
 }
