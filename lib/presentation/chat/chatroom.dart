47675d3c50113ddc0d8044ef3fbb8e9e886d4f54
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2025-04-02 20:11:20 +0530

    removed prefix icon

--- lib/presentation/chat/chatroom.dart
+++ lib/presentation/chat/chatroom.dart
@@ -265,8 +265,7 @@ class _ChatRoomState extends State<ChatRoom> {
                     cursorColor: kSelectedBackgroundColor,
                     style: const TextStyle(color: kWhite),
                     decoration: InputDecoration(
-                      prefixIcon:
-                          IconButton(onPressed: () {}, icon: Icon(Icons.link)),
+                  
                       filled: true,
                       hintText: 'Enter your message',
                       focusColor: kSelectedBackgroundColor,
